9a6bc6588c0f90a79235a676b892fa4e0e8d48a6
RobertoVallejo <vallejoroberto19@gmail.com>
2025-03-19 15:39:45 -0500

    Seccion 15: Avance

--- 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,27 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent { }
+export class SwitchesPageComponent {
+
+  formUtils = FormUtils
+  private fb = inject(FormBuilder)
+
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue]
+  })
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+    }
+
+}
