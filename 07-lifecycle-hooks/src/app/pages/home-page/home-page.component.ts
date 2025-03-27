b4d6ed932c43ad76eefce256e9835d23dd4937c3
RobertoVallejo <vallejoroberto19@gmail.com>
2025-03-27 00:09:48 -0500

    Seccion 16: LifeCycle Hooks

--- 07-lifecycle-hooks/src/app/pages/home-page/home-page.component.ts
+++ 07-lifecycle-hooks/src/app/pages/home-page/home-page.component.ts
@@ -1,44 +1,89 @@
-import { Component } from '@angular/core';
+import { afterNextRender, afterRender, Component, effect, signal } from '@angular/core';
+import { TittleComponent } from '../../components/tittle/tittle.component';
 
-const log =(...messages:string[]) => {}
+const log =(...messages:string[]) => {
+  console.log(`${ messages[0] } %c${messages.slice(1).join(', ')}`, 'color: #bada55');
+}
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [TittleComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
 
+  traditionalProperty = 'Roberto'
+  signalProperty = signal('Roberto')
+
   constructor(){
     console.log('constructor llamado');
+
+    // setTimeout(() => {
+    //   this.signalProperty.set('Roberto Vallejo');
+    //   console.log('hecho')
+    // }, 2000)
   }
 
+  changeTraditional(){
+    this.traditionalProperty = 'Roberto Vallejo';
+  }
+
+  changeSignal(){
+    this.signalProperty.set('Roberto Vallejo');
+  }
+
+  basicEffect = effect(( onCleanup) => {
+    log('Effect', 'Disparar efectos secundarios');
+
+    onCleanup(() => {
+      log('onCleanup', 'Se ejecuta cuando el efecto se va a destruir');
+    });
+  })
+
   ngOnInit(){
-    console.log('ngOnInit',
+    log('ngOnInit',
       "Runs once after Angular has initialized all the component's inputs.");
   }
   ngOnChanges(){
-    console.log('ngOnChanges',
+    log('ngOnChanges',
     "Runs every time the component's inputs have changed.");
   }
   ngDoCheck(){
-    console.log('ngDoCheck',
+    log('ngDoCheck',
     "Runs every time this component is checked for changes.");
   }
   ngAfterContentInit(){
-    console.log('ngAfterContentInit',
-    "Runs once after the component's content has been initialized.");
+    log(
+      'ngAfterContentInit',
+      "Runs once after the component's content has been initialized.");
   }
   ngAfterContentChecked(){
-    console.log('ngAfterContentChecked',
+    log('ngAfterContentChecked',
     "Runs every time this component content has been checked for changes.");
   }
   ngAfterViewInit(){
-    console.log('ngAfterViewInit',
+    log('ngAfterViewInit',
     "Runs once after the component's view has been initialized.");
   }
   ngAfterViewChecked(){
-    console.log('ngAfterViewChecked',
+    log('ngAfterViewChecked',
     "Runs every time the component's view has been checked for changes.");
   }
+
+  ngOnDestroy(){
+    log('ngOnDestroy',
+    "Runs once before Angular destroys the component.");
+  }
+
+  afterNextRenderEffecto = afterNextRender(() => {
+    log('afterNextRender',
+    "Runs once the next time that all components have been rendered to the DOM."
+    );
+  });
+
+  afterRender = afterRender(() => {
+    log('afterRender',
+    "Runs every time all components have been rendered to the DOM."
+    );
+  });
 }
