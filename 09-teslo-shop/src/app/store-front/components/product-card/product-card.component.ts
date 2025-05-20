4c79aca0c3eb1346edab3fe2b719c7ca4e725b92
Roberto Josafat Vallejo Reyes <vallejoroberto19@gmail.com>
2025-05-19 23:17:21 -0500

    Seccion 18: Avance Laptob

--- 09-teslo-shop/src/app/store-front/components/product-card/product-card.component.ts
+++ 09-teslo-shop/src/app/store-front/components/product-card/product-card.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'product-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent { }
