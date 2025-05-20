c6782814a2976cdc7821ce44cdc7d6a3fcbdf6b0
RobertoVallejo <vallejoroberto19@gmail.com>
2025-05-20 13:28:31 -0500

    Seccion 18: Avance

--- 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject  } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '@products/services/products.service';
 import { ProductCardComponent } from '@store-front/components/product-card/product-card.component';
 
 
@@ -7,4 +9,15 @@ import { ProductCardComponent } from '@store-front/components/product-card/produ
   imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {
+
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    request: () => ({}),
+    loader:({ request }) => {
+      return this.productsService.getProducts();
+    }
+  })
+
+ }
